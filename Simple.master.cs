e9dd1ee49aee5c0188be0614c4a508cdc31d977b
RCCService <102265925+RCCService@users.noreply.github.com>
2025-04-16 09:19:31 -0400

    Add files via upload

---
+++ Simple.master.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Roblox.Web.Base;
+using Roblox.Web.Code;
+
+namespace Roblox.Website
+{
+    public partial class Simple : SimpleMasterPageBase
+    {
+        protected override void Page_Init(object sender, EventArgs e) { }
+
+        protected override void Page_Load(object sender, EventArgs e)
+        {
+            RobloxCSS.PageCSS.Add("~/CSS/PartialViews/Navigation.css");
+            RobloxScripts.AddJQuery();
+            RobloxScripts.AddMicrosoftAjax();
+            RobloxScripts.BaseScripts.AddRange(new string[]
+            {
+                "~/js/jquery.simplemodal-1.3.5.js",
+                "~/js/jquery.tipsy.js",
+                "~/js/GoogleAnalytics/GoogleAnalyticsEvents.js",
+                "~/js/json2.min.js",
+                "~/js/jquery.cookie.js",
+                "~/js/polyfill/ie7localStorage.js",
+                "~/js/DropDownNav.js",
+                "~/js/JSErrorTracker.js",
+                "~/js/RobloxEventManager.js",
+                "~/js/RobloxEventListener.js",
+                "~/js/GoogleEventListener.js",
+                "~/js/MongoEventListener.js",
+                "~/js/SiteTouchEvent.js",
+                "~/js/GenericConfirmation.js",
+                "~/js/UpsellAdModal.js"
+            });
+        }
+    }
+}
---
+++ Test.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Simple.master" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="Roblox.Website.Test" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphRoblox" runat="server">
+	Page content goes here.
+</asp:Content>
